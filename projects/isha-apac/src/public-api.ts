2a02829df19b3d954e38c6009f7aed20a9aa8bb7
Ganga Jogur <ganga.jogur@gmail.com>
2022-04-09 13:54:34 +1000

    packaged for GPR

--- projects/isha-apac/src/public-api.ts
+++ projects/isha-apac/src/public-api.ts
@@ -2,6 +2,9 @@
  * Public API Surface of isha-apac
  */
 
-export * from './lib/isha-apac.service';
+export * from './lib/core-module/core-module.module';
+export * from './lib/core-module/services/backend.service';
+export * from './lib/core-module/services/date/dayjs/dayjs.date.service';
 export * from './lib/isha-apac.component';
 export * from './lib/isha-apac.module';
+export * from './lib/isha-apac.service';
