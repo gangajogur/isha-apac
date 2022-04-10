17dd9597dcdc4612a93f94606a0d7225fa2b7e6c
Ganga Jogur <ganga.jogur@gmail.com>
2022-04-10 17:11:13 +1000

    added console log for ide setup

--- projects/isha-apac/schematics/ide-setup/index.ts
+++ projects/isha-apac/schematics/ide-setup/index.ts
@@ -1,9 +1,11 @@
 import { normalize } from '@angular-devkit/core';
-import { apply, chain, mergeWith, move, Rule, url } from '@angular-devkit/schematics';
+import { apply, chain, mergeWith, move, Rule, SchematicContext, Tree, url } from '@angular-devkit/schematics';
 import { MergeStrategy } from '@angular-devkit/schematics/src/tree/interface';
 
 export function setupIde(): Rule {
-  return async () => {
+  // @ts-ignore
+  return async (host: Tree, context: SchematicContext) => {
+    context.logger.log('info', 'Setting the VSCode IDE');
     const templateSource = apply(url('../../../../.vscode'), [move(normalize(`./.vscode`))]);
     return chain([mergeWith(templateSource, MergeStrategy.Overwrite)]);
   };
--- projects/isha-apac/schematics/ng-add/index.ts
+++ projects/isha-apac/schematics/ng-add/index.ts
@@ -1,10 +1,13 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, schematic, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { BaseSchema } from '../base.schema';
 
 // Just return the tree
-export function ngAdd(): Rule {
+export function ngAdd(options: BaseSchema): Rule {
+  // @ts-ignore
   return (tree: Tree, context: SchematicContext) => {
     context.addTask(new NodePackageInstallTask());
+    schematic('ide-setup', options);
     return tree;
   };
 }
